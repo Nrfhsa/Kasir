5edd7f3fbd284a004fa18a1fd5a5caf14fafe815
Nrfhsa <nurfalahsyufa2@gmail.com>
2025-03-22 21:23:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require('path'); // Tambahkan modul path
 
 const app = express();
-const port = 8080;
+const port = 9000;
 
 // Setup direktori dan file database
 const dataDir = path.join(__dirname, 'data');
